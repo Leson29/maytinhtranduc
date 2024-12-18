ac31ddc420f49109c4e34a831ae73101c977abc3
maytinhtranduc <sonseo2k1@gmail.com>
2024-12-18 12:39:15 +0700

    add file Controller, css, fonts, Images, js, Model, Upload, View

---
+++ View/Carousel.php
@@ -0,0 +1,46 @@
+<div class="slider-area">
+            <!-- Slider -->
+            <div class="block-slider block-slider4">
+                <ul class="" id="bxslider-home4">
+                    <li>
+                        <img src="Upload/carousel_1.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                Mới nhất <span class="primary"> <strong></strong></span>
+                            </h2>
+                            <h4 class="caption subtitle">Mua sắm ngay!</h4>
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li><img src="Upload/carousel_2.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                Mới nhất <span class="primary"> <strong></strong></span>
+                            </h2>
+                            <h4 class="caption subtitle">Mua sắm ngay!</h4>
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li><img src="Upload/carousel_3.png" alt="Slide">
+                        <div class="caption-group">
+                            <h2 class="caption title">
+                                Mới nhất <span class="primary"> <strong></strong></span>
+                            </h2>
+                            <h4 class="caption subtitle">Mua sắm ngay!</h4>
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                    <li><img src="Upload/carousel_7.png" alt="Slide">
+                        <div class="caption-group">
+                          <h2 class="caption title">
+                                Mới nhất <span class="primary"> <strong></strong></span>
+                            </h2>
+                            <h4 class="caption subtitle">Mua sắm ngay!</h4>
+                            <a class="caption button-radius" href="index.php?mod=products&act=allproducts"><span class="icon"></span>Shop now</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <!-- ./Slider -->
+    </div>
+     <!-- End slider area -->
---
+++ View/User/Aboutus.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<section class="our-webcoderskull padding-lg">
+  <div class="container">
+    <div class="row heading heading-icon">
+        <h2>Team chúng tôi</h2>
+    </div>
+    <ul class="row">
+      <li class="col-12 col-md-6 col-lg-3">
+          <div class="cnt-block equal-hight" style="height: 349px;">
+            <figure><img src="http://www.webcoderskull.com/img/team4.png" class="img-responsive" alt=""></figure>
+            <h3><a href="https://www.facebook.com/lehaison01">Lê Hải Sơn</a></h3>
+            <p>43.01.104.186</p>
+            <ul class="follow-us clearfix">
+              <li><a href="https://www.facebook.com/trannqt"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+              <li><a href="https://twitter.com/?lang=vi"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.linkedin.com/"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+      </li>
+    </ul>
+  </div>
+</section>
